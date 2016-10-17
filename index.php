4e66e73fd1f148770b2dfa05a307acf6c2f34dd7
jamespakorn <jameshosxp@hotmail.com>
2016-10-17 16:01:37 +0700

    edit2

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
          echo 'Hello Github James';
+         echo 'test';
          echo '<hr>';
          
          $arr=['delphi','php','java','javacript'];
